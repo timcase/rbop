26aa339f47feccbb4285ca5519a8e4107d6c4554
Tim Case <tim@2drops.net>
2025-07-04 17:42:47 -0700

    Implement mninal to_h, as_json, and []

--- lib/rbop/item.rb
+++ lib/rbop/item.rb
@@ -6,6 +6,30 @@ module Rbop
 
     def initialize(raw_hash)
       @raw = raw_hash
+      @data = deep_dup(raw_hash)
+    end
+
+    def to_h
+      deep_dup(@data)
+    end
+
+    alias_method :as_json, :to_h
+
+    def [](key)
+      @data[key.to_s]
+    end
+
+    private
+
+    def deep_dup(obj)
+      case obj
+      when Hash
+        obj.transform_keys(&:to_s).transform_values { |v| deep_dup(v) }
+      when Array
+        obj.map { |v| deep_dup(v) }
+      else
+        obj.dup rescue obj
+      end
     end
   end
 end
---
+++ test/item_test.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class ItemTest < Minitest::Test
+  def test_to_h_returns_deep_copy_and_original_unchanged
+    original_hash = {
+      "id" => "abc123",
+      "title" => "My Login",
+      "vault" => { "id" => "vault123" },
+      "fields" => [{ "id" => "field1", "value" => "secret" }]
+    }
+    
+    item = Rbop::Item.new(original_hash)
+    copy = item.to_h
+    
+    # Modify the copy
+    copy["title"] = "Modified Title"
+    copy["vault"]["id"] = "modified_vault"
+    copy["fields"][0]["value"] = "modified_secret"
+    
+    # Original should be unchanged
+    assert_equal "My Login", item.raw["title"]
+    assert_equal "vault123", item.raw["vault"]["id"]
+    assert_equal "secret", item.raw["fields"][0]["value"]
+    
+    # Item's internal data should also be unchanged
+    assert_equal "My Login", item["title"]
+    assert_equal "vault123", item["vault"]["id"]
+  end
+
+  def test_bracket_access_works_with_strings_and_symbols
+    hash = { "id" => "abc123", "title" => "My Login" }
+    item = Rbop::Item.new(hash)
+    
+    assert_equal "abc123", item["id"]
+    assert_equal "abc123", item[:id]
+    assert_equal "My Login", item["title"]
+    assert_equal "My Login", item[:title]
+  end
+
+  def test_bracket_access_returns_nil_for_missing_keys
+    hash = { "id" => "abc123" }
+    item = Rbop::Item.new(hash)
+    
+    assert_nil item["missing"]
+    assert_nil item[:missing]
+  end
+
+  def test_as_json_is_alias_for_to_h
+    hash = { "id" => "abc123", "title" => "My Login" }
+    item = Rbop::Item.new(hash)
+    
+    assert_equal item.to_h, item.as_json
+    refute_same item.to_h, item.as_json  # Different objects
+  end
+
+  def test_deep_copy_handles_nested_structures
+    hash = {
+      "level1" => {
+        "level2" => {
+          "level3" => ["item1", "item2"]
+        }
+      }
+    }
+    
+    item = Rbop::Item.new(hash)
+    copy = item.to_h
+    
+    copy["level1"]["level2"]["level3"] << "item3"
+    
+    # Original should have only 2 items
+    assert_equal 2, item.raw["level1"]["level2"]["level3"].length
+    assert_equal 3, copy["level1"]["level2"]["level3"].length
+  end
+
+  def test_raw_attribute_preserves_original_hash
+    original_hash = { "id" => "abc123", :title => "My Login" }
+    item = Rbop::Item.new(original_hash)
+    
+    assert_same original_hash, item.raw
+    assert_equal "abc123", item.raw["id"]
+    assert_equal "My Login", item.raw[:title]
+  end
+end
